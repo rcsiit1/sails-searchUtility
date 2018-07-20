eefcb46092e9d3b78e08fc964a35e3ea43fcb4c5
rajan sharma <rajansharma9697@gmail.com>
2018-07-20 18:10:18 +0530

    changes done

--- api/controllers/SearchController.js
+++ api/controllers/SearchController.js
@@ -4,19 +4,22 @@ let searchUtility = async (req, res) => {
 };
 
 let search = async (req, res) => {
-  let q = req.body.search_query;
-  let category = req.body.category;
+  let q = req.param('search_query');
+  let category = req.param('category');
+  console.log(category);
   let query = {};
   let subquery = {};
   subquery['contains'] = q;
   query[category] = subquery;
   let results = await Csvtodatabase.find(query);
   console.log(results);
-  res.view('pages/searchResult',{ results: results});
+  res.json({ results: results});
 };
+
 module.exports = {
   searchUtility: searchUtility,
-  search: search
+  search: search,
+
 
 };
 
--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,6 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
+
 
 module.exports.routes = {
-
-
-  //  ╦ ╦╔═╗╔╗ ╔═╗╔═╗╔═╗╔═╗╔═╗
-  //  ║║║║╣ ╠╩╗╠═╝╠═╣║ ╦║╣ ╚═╗
-  //  ╚╩╝╚═╝╚═╝╩  ╩ ╩╚═╝╚═╝╚═╝
-
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
 
   '/': {
     view: 'pages/homepage'
@@ -37,38 +15,10 @@ module.exports.routes = {
     controller: 'SearchController',
     action: 'searchUtility'
   },
-  '/search/': {
+  '/search': {
 
     controller: 'SearchController',
     action: 'search'
-  }
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
-
-  //  ╔═╗╔═╗╦  ╔═╗╔╗╔╔╦╗╔═╗╔═╗╦╔╗╔╔╦╗╔═╗
-  //  ╠═╣╠═╝║  ║╣ ║║║ ║║╠═╝║ ║║║║║ ║ ╚═╗
-  //  ╩ ╩╩  ╩  ╚═╝╝╚╝═╩╝╩  ╚═╝╩╝╚╝ ╩ ╚═╝
-
-
-
-  //  ╦ ╦╔═╗╔╗ ╦ ╦╔═╗╔═╗╦╔═╔═╗
-  //  ║║║║╣ ╠╩╗╠═╣║ ║║ ║╠╩╗╚═╗
-  //  ╚╩╝╚═╝╚═╝╩ ╩╚═╝╚═╝╩ ╩╚═╝
-
-
-  //  ╔╦╗╦╔═╗╔═╗
-  //  ║║║║╚═╗║
-  //  ╩ ╩╩╚═╝╚═╝
-
+  },
 
 };
